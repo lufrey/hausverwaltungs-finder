91251615c0cc1b283f73ed245428f2306425f4fc
Malie <s0587010@htw-berlin.de>
2023-10-28 00:18:02 +0200

    Start header design, create homepage tile grid

---
+++ components/PageHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class=" flex justify-between items-center">
+        <a href="/" class="logo text-xl font-medium tracking-tighter text-main">ApartiFind</a>
+        <span class="tagline text-l font-light opacity-50">what's a housing crisis?</span>
+        <div class="nav_links flex items-center gap-8">
+            <div class="favorites inline-block relative">
+                <img src="../assets/heart_outline.svg" alt="Hier sind deine Favorites" class="favorites__image">
+                <span class="favorites__counter absolute text-s top">3</span>
+            </div>
+            <a href="" class="contribute border-2 rounded-full border-primary px-5 py-2">
+                <span>Mitwirken</span>
+                <img src="../assets/github-mark.svg" alt="Klicke um auf GitHub zu diesem Projekt beizutragen" class="github_logo w-6 ml-2 inline">
+            </a>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+    .favorites__counter {
+        /* centers the counter in the heart */
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-54%);
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@ const route = useRoute();
 </script>
 
 <template>
-  <div>
-    <h1 class="p-3 m-7">Nuxt Routing set up successfully!</h1>
+  <div class="layout">
+    <!-- <h1>Nuxt Routing set up successfully!</h1>
     <p>Current route: {{ route.path }}</p>
     <a
       href="https://nuxt.com/docs/getting-started/routing"
@@ -13,6 +13,44 @@ const route = useRoute();
     >
     <p>
       <a href="/page2">Go to page 2</a>
-    </p>
+    </p> -->
+    <div class="eyecatcher">eyecatcher</div>
+    <div class="apartmentlist">apartmentlist</div>
+    <div class="map_preview">map_preview</div>
+    <div class="decoration">decoration</div>
+    <div class="mailing_list">mailing_list</div>
   </div>
 </template>
+
+<style scoped>
+.layout {  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  gap: 0px 0px;
+  grid-auto-flow: row;
+  grid-template-areas:
+    "eyecatcher eyecatcher eyecatcher eyecatcher eyecatcher eyecatcher apartmentlist apartmentlist apartmentlist apartmentlist apartmentlist apartmentlist"
+    "eyecatcher eyecatcher eyecatcher eyecatcher eyecatcher eyecatcher apartmentlist apartmentlist apartmentlist apartmentlist apartmentlist apartmentlist"
+    "eyecatcher eyecatcher eyecatcher eyecatcher eyecatcher eyecatcher apartmentlist apartmentlist apartmentlist apartmentlist apartmentlist apartmentlist"
+    "eyecatcher eyecatcher eyecatcher eyecatcher eyecatcher eyecatcher apartmentlist apartmentlist apartmentlist apartmentlist apartmentlist apartmentlist"
+    "eyecatcher eyecatcher eyecatcher eyecatcher eyecatcher eyecatcher apartmentlist apartmentlist apartmentlist apartmentlist apartmentlist apartmentlist"
+    "eyecatcher eyecatcher eyecatcher eyecatcher eyecatcher eyecatcher apartmentlist apartmentlist apartmentlist apartmentlist apartmentlist apartmentlist"
+    "decoration decoration decoration decoration map_preview map_preview apartmentlist apartmentlist apartmentlist apartmentlist apartmentlist apartmentlist"
+    "decoration decoration decoration decoration map_preview map_preview apartmentlist apartmentlist apartmentlist apartmentlist apartmentlist apartmentlist"
+    "decoration decoration decoration decoration . . . . . . . ."
+    "decoration decoration decoration decoration mailing_list mailing_list mailing_list mailing_list mailing_list mailing_list mailing_list mailing_list"
+    "decoration decoration decoration decoration mailing_list mailing_list mailing_list mailing_list mailing_list mailing_list mailing_list mailing_list"
+    "decoration decoration decoration decoration mailing_list mailing_list mailing_list mailing_list mailing_list mailing_list mailing_list mailing_list";
+}
+
+.eyecatcher { grid-area: eyecatcher; }
+
+.apartmentlist { grid-area: apartmentlist; }
+
+.map_preview { grid-area: map_preview; }
+
+.decoration { grid-area: decoration; }
+
+.mailing_list { grid-area: mailing_list; }
+
+</style>
