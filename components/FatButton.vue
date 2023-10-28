1a54e2d6fc4d144fd5365cb9da5dc0531c6e9ddb
Malie <s0587010@htw-berlin.de>
2023-10-28 20:56:37 +0200

    Create Button component for submit and navigation

---
+++ components/FatButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <button
+    class="absolute -right-10 -bottom-5 rounded-xl border border-primary bg-white px-6 py-4 text-m font-medium text-primary"
+    @click="handleClick"
+  >
+    {{ buttonText }}
+    <img src="/arrow_right.svg" alt="" class="inline ml-4">
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonText: {
+      type: String,
+      required: true,
+    },
+    action: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ["submit"],
+  methods: {
+    handleClick() {
+      if (this.action === "submit") {
+        // Submit form
+        this.$emit("submit");
+      } else {
+        // Navigate to new page
+        this.$router.push("/" + this.action);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  box-shadow: #a555a2 -7px 7px;
+}
+</style>
