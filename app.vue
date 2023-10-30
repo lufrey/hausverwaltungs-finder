6a5adcf76541b1cd7124fd7b3378eeee3e32d239
Lukas Frey <lukas.frey03@outlook.de>
2023-10-30 01:35:05 +0100

    add og image

--- app.vue
+++ app.vue
@@ -1,3 +1,65 @@
+<script setup lang="ts">
+const currentUrl = useRequestURL().origin;
+useHead({
+  title: "ApartiFind - Berlins Wohnungsmarkt auf einen Blick",
+  meta: [
+    {
+      hid: "description",
+      name: "description",
+      content:
+        "ApartiFind - Berlins Wohnungsmarkt auf einen Blick. Finde die passende Hausverwaltung für deine Wohnung.",
+    },
+    {
+      hid: "og:title",
+      property: "og:title",
+      content: "ApartiFind - Berlins Wohnungsmarkt auf einen Blick",
+    },
+    {
+      hid: "og:description",
+      property: "og:description",
+      content:
+        "ApartiFind - Berlins Wohnungsmarkt auf einen Blick. Finde die passende Hausverwaltung für deine Wohnung.",
+    },
+    {
+      hid: "og:image",
+      property: "og:image",
+      content: `${currentUrl}/og-image.png`,
+    },
+    {
+      hid: "og:url",
+      property: "og:url",
+      content: currentUrl,
+    },
+    {
+      hid: "twitter:title",
+      property: "twitter:title",
+      content: "ApartiFind - Berlins Wohnungsmarkt auf einen Blick",
+    },
+    {
+      hid: "twitter:description",
+      property: "twitter:description",
+      content:
+        "ApartiFind - Berlins Wohnungsmarkt auf einen Blick. Finde die passende Hausverwaltung für deine Wohnung.",
+    },
+    {
+      hid: "twitter:image",
+      property: "twitter:image",
+      content: `${currentUrl}/og-image.png`,
+    },
+    {
+      hid: "twitter:card",
+      property: "twitter:card",
+      content: "summary_large_image",
+    },
+    {
+      hid: "lang",
+      property: "lang",
+      content: "de",
+    },
+  ],
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
