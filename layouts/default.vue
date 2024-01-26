76137bded7909c8673db5304c713c6b7e7dea2d3
Malie Jona Bertram <malie.bertram@student.htw-berlin.de>
2024-01-26 20:53:53 +0100

    Add credit section

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,8 +9,22 @@
     <main class="grow">
       <slot />
     </main>
-    <footer class="flex justify-between pb-10">
+    <footer class="justify-between pb-10 lg:flex">
       <div>{{ new Date().getFullYear() }} © ApartiFind</div>
+      <br />
+      <aside class="block opacity-60">
+        <h6>Credits</h6>
+        <ul class="ml-4 list-disc">
+          <li>
+            <a
+              href="https://www.flaticon.com/free-icons/temperature"
+              target="_blank"
+              title="temperature icons"
+              >Temperature icons created by Freepik - Flaticon</a
+            >
+          </li>
+        </ul>
+      </aside>
     </footer>
   </div>
 </template>
